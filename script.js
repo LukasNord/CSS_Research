aa2443c601ac0c43fa63721fd9cb1dbf68eb597a
Lukas Nord <luke.nord@gmail.com>
2017-12-13 21:29:49 -0600

    css homework

---
+++ script.js
@@ -0,0 +1,47 @@
+$(document).ready(startUp);
+
+
+//console.log('JS');
+
+
+function startUp(){
+  //console.log('JQ');
+  $('#button').on( 'click', blurFunction);
+  $('#reset').on( 'click', reset);
+
+  $('#hueButton').on('click', testFunc);
+
+
+}//end startUp
+
+
+function blurFunction(){
+    console.log('inside my blur function');
+  $('img').css("filter","blur(2px)");
+
+}//end blur
+
+function reset(){
+  console.log('reset!');
+  $('img').css('filter', 'blur(0px)');
+}//end reset
+
+function testFunc(){
+  console.log('in rotate');
+  $('img').css("filter","hue-rotate(180deg)");
+}
+
+
+
+
+
+/*
+hue-rotate(120deg);
+
+("#btnid").click(function(){
+  $('body').css("filter","blur(2px)");
+});
+
+
+
+*/
